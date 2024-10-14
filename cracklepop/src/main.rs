cf4e140a67988a707d2f7c2eb184c750f168c8fd
Sachin Iyer <sachinjiyer@gmail.com>
2024-10-13 18:44:45 -0700

    draft 1

--- cracklepop/src/main.rs
+++ cracklepop/src/main.rs
@@ -11,3 +11,22 @@ fn main() {
         )
     });
 }
+
+// Saw https://alicja.dev/blog/programming/rc/2017/01/31/return-programmer.html
+// Seemed like a good quick test
+fn recursive(i: Option<u8>) {
+    let i = i.unwrap_or(1);
+    if i > 100 {
+        return;
+    }
+    println!(
+        "{}",
+        match (i % 3, i % 5) {
+            (0, 0) => "CracklePop".to_string(),
+            (0, _) => "Crackle".to_string(),
+            (_, 0) => "Pop".to_string(),
+            _ => i.to_string(),
+        }
+    );
+    recursive(Some(i + 1));
+}
